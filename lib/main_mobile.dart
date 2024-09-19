7e9d68675dd41fb9db2b3ed34c0f6cb1d7358f76
Mario Osama <114400410+MarioOsama@users.noreply.github.com>
2024-09-19 12:43:23 +0300

    Finish Sentry Installing✅
    - Added `sentry_flutter` to pubspec.yaml file.
    - Initialized the sentry in `main_desktop` and `main_mobile` files.

--- lib/main_mobile.dart
+++ lib/main_mobile.dart
@@ -4,6 +4,7 @@ import 'package:el_sharq_clinic/core/di/dependency_injection.dart';
 import 'package:el_sharq_clinic/core/routing/app_router.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,12 +18,20 @@ void main() async {
   // Dependencies Injection
   setupGetIt();
 
-  runApp(
-    EasyLocalization(
-      supportedLocales: const [Locale('en'), Locale('ar')],
-      path: 'assets/translations',
-      fallbackLocale: const Locale('en'),
-      child: MobileClinicSystem(appRouter: AppRouter()),
+  // Initialize Sentry
+  await SentryFlutter.init(
+    (options) {
+      options.dsn =
+          'https://dce18842c8416c46a9e283c8eec3b953@o4507978849779712.ingest.us.sentry.io/4507978857316352';
+      options.tracesSampleRate = 0.01;
+    },
+    appRunner: () => runApp(
+      EasyLocalization(
+        supportedLocales: const [Locale('en'), Locale('ar')],
+        path: 'assets/translations',
+        fallbackLocale: const Locale('en'),
+        child: MobileClinicSystem(appRouter: AppRouter()),
+      ),
     ),
   );
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,8 +8,10 @@ import Foundation
 import cloud_firestore
 import connectivity_plus
 import firebase_core
+import package_info_plus
 import printing
 import screen_retriever
+import sentry_flutter
 import shared_preferences_foundation
 import window_manager
 
@@ -17,8 +19,10 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
   ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
+  SentryFlutterPlugin.register(with: registry.registrar(forPlugin: "SentryFlutterPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
